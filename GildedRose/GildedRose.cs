e787b10fa198a36f5507e600035e02671bc931b3
Deeptinaik2001 <deeptinaik532001@gmail.com>
2023-09-29 11:01:58 +0530

    First commit

---
+++ GildedRose/GildedRose.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using GildedRoseKata;
+namespace csharp
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+       private const int MAX_QUALITY = 50;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+        private readonly Dictionary<string, Action<Item>> Products = new Dictionary<string, Action<Item>>()
+        {
+            {"Aged Brie",AgedBrie},
+            {"Sulfuras, Hand of Ragnaros", Sulfuras},
+            {"Backstage passes to a TAFKAL80ETC concert",BackStagePass},
+            {"Conjured Mana Cake",ConjuredItem },
+        };
+        public void UpdateQuality()
+        {
+            foreach (var item in Items)
+            {
+                (Products.TryGetValue(item.Name, out var result) ? result : RegularItem)(item);
+            }
+        }
+        private static void UpdateItem(Item item, int qualityChange)
+        {
+            item.SellIn--;
+            item.Quality += qualityChange;
+            if (item.SellIn < 0)
+            {
+                item.Quality += qualityChange;
+            }
+            if (item.Quality > MAX_QUALITY)
+            {
+                item.Quality = MAX_QUALITY;
+            }
+            if (item.Quality < 0)
+            {
+                item.Quality = 0;
+            }
+        }
+        private static void BackStagePass(Item item)
+        {
+            item.Quality++;
+            if (item.SellIn < 11)
+                item.Quality++;
+            if (item.SellIn < 6)
+                item.Quality++;
+            item.SellIn--;
+            if (item.SellIn < 0)
+                item.Quality = 0;
+            if (item.Quality > MAX_QUALITY)
+                item.Quality = MAX_QUALITY;
+        }
+        private static void AgedBrie(Item item) => UpdateItem(item, 1);
+        private static void Sulfuras(Item item){}
+        private static void RegularItem(Item item) => UpdateItem(item, -1);
+        private static void ConjuredItem(Item item) => UpdateItem(item, -2);
+    }
+}
+
---
+++ GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,88 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+using csharp;
+using Xunit.Abstractions;
+
+namespace GildedRoseTests
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "fixme", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal("fixme", Items[0].Name);
+
+            
+        }
+
+            IList<Item> Items = new List<Item>{
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
+
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                          
+
+    };
+
+        [Fact]
+        public void Test()
+        {
+            var app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(4, Items[7].SellIn);
+            Assert.Equal(50, Items[7].Quality);
+
+        }
+        [Fact]
+        public void Day15()
+        {
+            var app = new GildedRose(Items);
+            for (int i = 0; i < 15; i++)
+            app.UpdateQuality();
+            Assert.Equal(0, Items[3].SellIn);
+            Assert.Equal(80, Items[3].Quality);
+
+        }
+
+        [Fact]
+        public void Day30()
+        {
+            var app = new GildedRose(Items);
+            for (int i = 0; i < 30; i++)
+                app.UpdateQuality();
+            Assert.Equal(-20, Items[0].SellIn);
+            Assert.Equal(0, Items[0].Quality);
+
+            Assert.Equal(-25, Items[7].SellIn);
+            Assert.Equal(0, Items[7].Quality);
+
+        }
+        
+
+    }
+}
