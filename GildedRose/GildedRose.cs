b56d17e05e09b7055a188750218622c08a575b98
Deeptinaik2001 <deeptinaik532001@gmail.com>
2023-10-04 10:43:04 +0530

    second commit

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -6,11 +6,8 @@ namespace csharp
     public class GildedRose
     {
         IList<Item> Items;
-       private const int MAX_QUALITY = 50;
-        public GildedRose(IList<Item> Items)
-        {
-            this.Items = Items;
-        }
+        private const int MAX_QUALITY = 50;
+        public GildedRose(IList<Item> Items) => this.Items = Items;
         private readonly Dictionary<string, Action<Item>> Products = new Dictionary<string, Action<Item>>()
         {
             {"Aged Brie",AgedBrie},
@@ -21,42 +18,23 @@ namespace csharp
         public void UpdateQuality()
         {
             foreach (var item in Items)
-            {
                 (Products.TryGetValue(item.Name, out var result) ? result : RegularItem)(item);
-            }
         }
         private static void UpdateItem(Item item, int qualityChange)
         {
             item.SellIn--;
-            item.Quality += qualityChange;
-            if (item.SellIn < 0)
-            {
-                item.Quality += qualityChange;
-            }
-            if (item.Quality > MAX_QUALITY)
-            {
-                item.Quality = MAX_QUALITY;
-            }
-            if (item.Quality < 0)
-            {
-                item.Quality = 0;
-            }
+            int updatedQuality = item.Quality + qualityChange + (item.SellIn < 0 ? qualityChange : 0);
+            item.Quality = Math.Max(0, Math.Min(updatedQuality, MAX_QUALITY));
         }
         private static void BackStagePass(Item item)
         {
-            item.Quality++;
-            if (item.SellIn < 11)
-                item.Quality++;
-            if (item.SellIn < 6)
-                item.Quality++;
+            item.Quality += (item.SellIn <= 5) ? 3 : (item.SellIn <= 10) ? 2 : 1;
             item.SellIn--;
-            if (item.SellIn < 0)
-                item.Quality = 0;
-            if (item.Quality > MAX_QUALITY)
-                item.Quality = MAX_QUALITY;
+            if (item.SellIn < 0) item.Quality = 0;
+            item.Quality = Math.Clamp(item.Quality, 0, MAX_QUALITY);
         }
         private static void AgedBrie(Item item) => UpdateItem(item, 1);
-        private static void Sulfuras(Item item){}
+        private static void Sulfuras(Item item) { }
         private static void RegularItem(Item item) => UpdateItem(item, -1);
         private static void ConjuredItem(Item item) => UpdateItem(item, -2);
     }
